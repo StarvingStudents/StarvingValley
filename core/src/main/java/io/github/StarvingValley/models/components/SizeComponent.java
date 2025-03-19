5fcc7f5864f57cf8e463f7a1413fd85a55bd1b3f
Andrefkl <andre.klarpaas@gmail.com>
2025-03-19 23:52:24 +0100

    add size component

---
+++ core/src/main/java/io/github/StarvingValley/models/components/SizeComponent.java
@@ -0,0 +1,13 @@
+package io.github.StarvingValley.models.components;
+
+import com.badlogic.ashley.core.Component;
+
+public class SizeComponent implements Component {
+  public float width;
+  public float height;
+
+  public SizeComponent(float width, float height) {
+    this.width = width;
+    this.height = height;
+  }
+}
