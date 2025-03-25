7086fa335b85f7308b3dbd64a84eee4d9933b6de
Kristian Seon-Ho Kim <kskim@stud.ntnu.no>
2025-03-25 08:16:55 +0100

    feat(component): add EatingComponent to manage food points

---
+++ core/src/main/java/io/github/StarvingValley/models/components/EatingComponent.java
@@ -0,0 +1,7 @@
+package io.github.StarvingValley.models.components;
+
+import com.badlogic.ashley.core.Component;
+
+public class EatingComponent implements Component {
+    public float food_points;
+}
