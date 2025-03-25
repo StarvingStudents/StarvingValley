b68071bb3222359cbe976ebab1b68de94843d771
Kristian Seon-Ho Kim <kskim@stud.ntnu.no>
2025-03-25 08:14:52 +0100

    feat(config): add hunger properties

--- core/src/main/java/io/github/StarvingValley/config/Config.java
+++ core/src/main/java/io/github/StarvingValley/config/Config.java
@@ -2,4 +2,7 @@ package io.github.StarvingValley.config;
 
 public class Config {
     public static final String FIREBASE_DATABASE_URL = "https://starving-valley-default-rtdb.europe-west1.firebasedatabase.app";
+
+    public static final float max_hunger_points = 100; 
+    public static final float hunger_length = 3600; // In seconds
 }
