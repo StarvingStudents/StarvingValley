0a56a7a8b8f1b422a0205d57b79e335fd970a7ea
Kristian Seon-Ho Kim <kskim@stud.ntnu.no>
2025-03-27 13:37:21 +0100

    fix(hunger): initialize hungerPoints with MAX_HUNGER_POINTS

--- core/src/main/java/io/github/StarvingValley/models/components/HungerComponent.java
+++ core/src/main/java/io/github/StarvingValley/models/components/HungerComponent.java
@@ -5,7 +5,7 @@ import com.badlogic.ashley.core.Component;
 import io.github.StarvingValley.config.Config;
 
 public class HungerComponent implements Component {
-    public float hungerPoints; 
+    public float hungerPoints = Config.MAX_HUNGER_POINTS; 
     public float maxHungerPoints = Config.MAX_HUNGER_POINTS;
     public float decayRate = Config.MAX_HUNGER_POINTS / Config.HUNGER_LENGTH;
 }
