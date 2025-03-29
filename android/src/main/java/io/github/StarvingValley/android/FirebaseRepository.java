49cd3b9c8df13f30a0cae9c1e5b68166598ff349
Laurenz Kirchmayr <kirchmayr.laurenz@gmail.com>
2025-03-29 10:59:43 +0100

    adds firebase auth integration

--- android/src/main/java/io/github/StarvingValley/android/FirebaseRepository.java
+++ android/src/main/java/io/github/StarvingValley/android/FirebaseRepository.java
@@ -2,7 +2,11 @@ package io.github.StarvingValley.android;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.concurrent.Executor;
+
+import io.github.StarvingValley.models.Interfaces.AuthCallback;
 import io.github.StarvingValley.models.Interfaces.IFirebaseRepository;
 import io.github.StarvingValley.config.Config;
 
@@ -10,14 +14,57 @@ public class FirebaseRepository implements IFirebaseRepository {
 
     FirebaseDatabase _database;
     DatabaseReference _users;
+    FirebaseAuth _auth;
 
     public FirebaseRepository() {
         _database = FirebaseDatabase.getInstance(Config.FIREBASE_DATABASE_URL);
         _users = _database.getReference("users");
+        _auth = FirebaseAuth.getInstance();
     }
 
     @Override
     public void SubmitUser(String username) {
         _users.push().setValue(username);
+    }
+
+    @Override
+    public void signInWithEmail(String email, String password, AuthCallback callback) {
+        _auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    callback.onSuccess();
+                } else {
+                    String errorMessage = task.getException() != null ? task.getException().getMessage() : "Unknown error";
+                    callback.onFailure(errorMessage);
+                }
+            });
+    }
+
+    @Override
+    public void signOut() {
+        _auth.signOut();
+    }
+
+    @Override
+    public boolean isSignedIn() {
+        return _auth.getCurrentUser() != null;
+    }
+
+    @Override
+    public String getCurrentUserId() {
+        return _auth.getCurrentUser() != null ? _auth.getCurrentUser().getUid() : null;
+    }
+
+    @Override
+    public void registerWithEmail(String email, String password, AuthCallback callback) {
+        _auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    callback.onSuccess();
+                } else {
+                    String errorMessage = task.getException() != null ? task.getException().getMessage() : "Unknown error";
+                    callback.onFailure(errorMessage);
+                }
+            });
     }
 }
