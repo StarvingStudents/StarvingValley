1a8fbd21b35fa4c0a2260df8bda4a8e6d172cf33
Kristian Seon-Ho Kim <kskim@stud.ntnu.no>
2025-04-01 15:33:52 +0200

    Remove fields isEating and eatingSpeed from EatingComponent

--- core/src/main/java/io/github/StarvingValley/models/components/EatingComponent.java
+++ core/src/main/java/io/github/StarvingValley/models/components/EatingComponent.java
@@ -4,6 +4,6 @@ import com.badlogic.ashley.core.Component;
 
 public class EatingComponent implements Component {
     public float foodPoints; // Remaining number of food points of food item which may be added to the player
-    public boolean isEating; // Whether the player is currently eating
-    public float eatingSpeed; // Number of food points added per second
+    // public boolean isEating; // Whether the player is currently eating
+    // public float eatingSpeed; // Number of food points added per second
 }
