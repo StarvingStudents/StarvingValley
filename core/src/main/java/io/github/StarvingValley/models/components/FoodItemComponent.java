ed58c711f27745efec641c4ecaf7c54d99814604
Kristian Seon-Ho Kim <kskim@stud.ntnu.no>
2025-04-01 15:34:32 +0200

    Add FoodItemComponent to represent food items and their properties

---
+++ core/src/main/java/io/github/StarvingValley/models/components/FoodItemComponent.java
@@ -0,0 +1,9 @@
+package io.github.StarvingValley.models.components;
+
+import com.badlogic.ashley.core.Component;
+
+public class FoodItemComponent implements Component {
+    public float foodPoints; // Number of food points of food item which may be added to the player
+    public float eatingSpeed; // Number of food points added per second
+    public boolean isHeld; // Whether the food item is currently held by the player
+}
