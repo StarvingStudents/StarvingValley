4844340c3fcdcbedc12846f7bd7525d9102c01b7
Kristian Seon-Ho Kim <kskim@stud.ntnu.no>
2025-04-01 15:20:16 +0200

    Add fields for isEating, eatingSpeed

--- core/src/main/java/io/github/StarvingValley/models/components/EatingComponent.java
+++ core/src/main/java/io/github/StarvingValley/models/components/EatingComponent.java
@@ -3,5 +3,7 @@ package io.github.StarvingValley.models.components;
 import com.badlogic.ashley.core.Component;
 
 public class EatingComponent implements Component {
-    public float foodPoints;
+    public float foodPoints; // Remaining number of food points of food item which may be added to the player
+    public boolean isEating; // Whether the player is currently eating
+    public float eatingSpeed; // Number of food points added per second
 }
