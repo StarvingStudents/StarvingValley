c851db7fb46d18a138fb1a2d326dbb5ad0b19bc7
Kristian Seon-Ho Kim <kskim@stud.ntnu.no>
2025-04-10 14:19:56 +0200

    Readd Componentmappers

--- core/src/main/java/io/github/StarvingValley/models/Mappers.java
+++ core/src/main/java/io/github/StarvingValley/models/Mappers.java
@@ -41,60 +41,67 @@ import io.github.StarvingValley.models.components.WorldLayerComponent;
 import io.github.StarvingValley.models.components.WorldMapFarmComponent;
 
 public class Mappers {
-    public static final ComponentMapper<PositionComponent> position = ComponentMapper
-            .getFor(PositionComponent.class);
-    public static final ComponentMapper<VelocityComponent> velocity = ComponentMapper
-            .getFor(VelocityComponent.class);
-    public static final ComponentMapper<SizeComponent> size = ComponentMapper.getFor(SizeComponent.class);
-    public static final ComponentMapper<CollidableComponent> collidable = ComponentMapper
-            .getFor(CollidableComponent.class);
-    public static final ComponentMapper<EnvironmentCollidableComponent> environmentCollider = ComponentMapper
-            .getFor(EnvironmentCollidableComponent.class);
-    public static final ComponentMapper<CameraComponent> camera = ComponentMapper.getFor(CameraComponent.class);
-    public static final ComponentMapper<CameraFollowComponent> cameraFollow = ComponentMapper
-            .getFor(CameraFollowComponent.class);
-    public static final ComponentMapper<UnsyncedComponent> unsynced = ComponentMapper
-            .getFor(UnsyncedComponent.class);
-    public static final ComponentMapper<SyncComponent> sync = ComponentMapper.getFor(SyncComponent.class);
-    public static final ComponentMapper<MapRenderComponent> mapRender = ComponentMapper
-            .getFor(MapRenderComponent.class);
-    public static final ComponentMapper<EatingComponent> eating = ComponentMapper.getFor(EatingComponent.class);
-    public static final ComponentMapper<PlayerComponent> player = ComponentMapper.getFor(PlayerComponent.class);
-    public static final ComponentMapper<SpeedComponent> speed = ComponentMapper.getFor(SpeedComponent.class);
-    public static final ComponentMapper<ActiveWorldEntityComponent> activeWorldEntity = ComponentMapper
-                    .getFor(ActiveWorldEntityComponent.class);
-    public static final ComponentMapper<InputComponent> input = ComponentMapper.getFor(InputComponent.class);
-    public static final ComponentMapper<ClickableComponent> clickable = ComponentMapper
-                    .getFor(ClickableComponent.class);
-    public static final ComponentMapper<ClickedComponent> clicked = ComponentMapper
-                    .getFor(ClickedComponent.class);
-    public static final ComponentMapper<SpriteComponent> sprite = ComponentMapper.getFor(SpriteComponent.class);
-    public static final ComponentMapper<TiledMapComponent> tiledMap = ComponentMapper
-                    .getFor(TiledMapComponent.class);
-    public static final ComponentMapper<TileOccupierComponent> tileOccupier = ComponentMapper
-            .getFor(TileOccupierComponent.class);
-    public static final ComponentMapper<BuildPreviewComponent> buildPreview = ComponentMapper
-            .getFor(BuildPreviewComponent.class);
-    public static final ComponentMapper<HiddenComponent> hidden = ComponentMapper.getFor(HiddenComponent.class);
-    public static final ComponentMapper<PulseAlphaComponent> pulseAlpha = ComponentMapper
-                    .getFor(PulseAlphaComponent.class);
-    public static final ComponentMapper<WorldLayerComponent> worldLayer = ComponentMapper
-            .getFor(WorldLayerComponent.class);
-    public static final ComponentMapper<BuildableComponent> buildable = ComponentMapper
-            .getFor(BuildableComponent.class);
-    public static final ComponentMapper<HungerComponent> hunger = ComponentMapper.getFor(HungerComponent.class);
-    public static final ComponentMapper<DurabilityComponent> durability = ComponentMapper
-            .getFor(DurabilityComponent.class);
-    public static final ComponentMapper<GrowthStageComponent> growthStage = ComponentMapper
-            .getFor(GrowthStageComponent.class);
-    public static final ComponentMapper<TimeToGrowComponent> timeToGrow = ComponentMapper
-            .getFor(TimeToGrowComponent.class);
-    public static final ComponentMapper<HarvestingComponent> harvesting = ComponentMapper
-            .getFor(HarvestingComponent.class);
-    public static final ComponentMapper<CropTypeComponent> cropType = ComponentMapper.getFor(CropTypeComponent.class);
-    public static final ComponentMapper<SyncDeletionRequestComponent> syncDeletionRequest = ComponentMapper
-                    .getFor(SyncDeletionRequestComponent.class);
-    public static final ComponentMapper<DropComponent> drop = ComponentMapper.getFor(DropComponent.class);
-    public static final ComponentMapper<WorldMapFarmComponent> worldMapFarm = ComponentMapper
-                    .getFor(WorldMapFarmComponent.class);
+        public static final ComponentMapper<PositionComponent> position = ComponentMapper
+                        .getFor(PositionComponent.class);
+        public static final ComponentMapper<VelocityComponent> velocity = ComponentMapper
+                        .getFor(VelocityComponent.class);
+        public static final ComponentMapper<SizeComponent> size = ComponentMapper.getFor(SizeComponent.class);
+        public static final ComponentMapper<CollidableComponent> collidable = ComponentMapper
+                        .getFor(CollidableComponent.class);
+        public static final ComponentMapper<EnvironmentCollidableComponent> environmentCollider = ComponentMapper
+                        .getFor(EnvironmentCollidableComponent.class);
+        public static final ComponentMapper<CameraComponent> camera = ComponentMapper.getFor(CameraComponent.class);
+        public static final ComponentMapper<CameraFollowComponent> cameraFollow = ComponentMapper
+                        .getFor(CameraFollowComponent.class);
+        public static final ComponentMapper<UnsyncedComponent> unsynced = ComponentMapper
+                        .getFor(UnsyncedComponent.class);
+        public static final ComponentMapper<SyncComponent> sync = ComponentMapper.getFor(SyncComponent.class);
+        public static final ComponentMapper<MapRenderComponent> mapRender = ComponentMapper
+                        .getFor(MapRenderComponent.class);
+        public static final ComponentMapper<EatingComponent> eating = ComponentMapper.getFor(EatingComponent.class);
+        public static final ComponentMapper<PlayerComponent> player = ComponentMapper.getFor(PlayerComponent.class);
+        public static final ComponentMapper<SpeedComponent> speed = ComponentMapper.getFor(SpeedComponent.class);
+        public static final ComponentMapper<ActiveWorldEntityComponent> activeWorldEntity = ComponentMapper
+                        .getFor(ActiveWorldEntityComponent.class);
+        public static final ComponentMapper<InputComponent> input = ComponentMapper.getFor(InputComponent.class);
+        public static final ComponentMapper<ClickableComponent> clickable = ComponentMapper
+                        .getFor(ClickableComponent.class);
+        public static final ComponentMapper<ClickedComponent> clicked = ComponentMapper
+                        .getFor(ClickedComponent.class);
+        public static final ComponentMapper<SpriteComponent> sprite = ComponentMapper.getFor(SpriteComponent.class);
+        public static final ComponentMapper<TiledMapComponent> tiledMap = ComponentMapper
+                        .getFor(TiledMapComponent.class);
+        public static final ComponentMapper<TileOccupierComponent> tileOccupier = ComponentMapper
+                        .getFor(TileOccupierComponent.class);
+        public static final ComponentMapper<BuildPreviewComponent> buildPreview = ComponentMapper
+                        .getFor(BuildPreviewComponent.class);
+        public static final ComponentMapper<HiddenComponent> hidden = ComponentMapper.getFor(HiddenComponent.class);
+        public static final ComponentMapper<PulseAlphaComponent> pulseAlpha = ComponentMapper
+                        .getFor(PulseAlphaComponent.class);
+        public static final ComponentMapper<WorldLayerComponent> worldLayer = ComponentMapper
+                        .getFor(WorldLayerComponent.class);
+        public static final ComponentMapper<BuildableComponent> buildable = ComponentMapper
+                        .getFor(BuildableComponent.class);
+        public static final ComponentMapper<HungerComponent> hunger = ComponentMapper.getFor(HungerComponent.class);
+        public static final ComponentMapper<DurabilityComponent> durability = ComponentMapper
+                        .getFor(DurabilityComponent.class);
+        public static final ComponentMapper<GrowthStageComponent> growthStage = ComponentMapper
+                        .getFor(GrowthStageComponent.class);
+        public static final ComponentMapper<TimeToGrowComponent> timeToGrow = ComponentMapper
+                        .getFor(TimeToGrowComponent.class);
+        public static final ComponentMapper<HarvestingComponent> harvesting = ComponentMapper
+                        .getFor(HarvestingComponent.class);
+        public static final ComponentMapper<CropTypeComponent> cropType = ComponentMapper
+                        .getFor(CropTypeComponent.class);
+        public static final ComponentMapper<SyncDeletionRequestComponent> syncDeletionRequest = ComponentMapper
+                        .getFor(SyncDeletionRequestComponent.class);
+        public static final ComponentMapper<DropComponent> drop = ComponentMapper.getFor(DropComponent.class);
+        public static final ComponentMapper<WorldMapFarmComponent> worldMapFarm = ComponentMapper
+                        .getFor(WorldMapFarmComponent.class);
+        public static final ComponentMapper<AnimationComponent> animation = ComponentMapper
+                        .getFor(AnimationComponent.class);
+        public static final ComponentMapper<TradingComponent> trading = ComponentMapper
+                        .getFor(TradingComponent.class);
+        public static final ComponentMapper<EconomyComponent> economy = ComponentMapper
+                        .getFor(EconomyComponent.class);
 }
