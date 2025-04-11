c83d2ae08f4006dca47681ab96605737f8ddf59f
Laurenz Kirchmayr <kirchmayr.laurenz@gmail.com>
2025-04-11 11:24:43 +0200

    adds village view and controller

---
+++ core/src/main/java/io/github/StarvingValley/controllers/VillageController.java
@@ -0,0 +1,113 @@
+package io.github.StarvingValley.controllers;
+
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import io.github.StarvingValley.config.Config;
+import io.github.StarvingValley.models.Mappers;
+import io.github.StarvingValley.models.Interfaces.IFirebaseRepository;
+import io.github.StarvingValley.models.components.CameraComponent;
+import io.github.StarvingValley.models.components.TiledMapComponent;
+import io.github.StarvingValley.models.entities.CameraFactory;
+import io.github.StarvingValley.models.entities.MapFactory;
+import io.github.StarvingValley.models.events.EventBus;
+import io.github.StarvingValley.models.systems.CameraSystem;
+import io.github.StarvingValley.models.systems.ClickSystem;
+import io.github.StarvingValley.models.systems.ClickedCleanupSystem;
+import io.github.StarvingValley.models.systems.EnvironmentCollisionSystem;
+import io.github.StarvingValley.models.systems.EventCleanupSystem;
+import io.github.StarvingValley.models.systems.FirebaseSyncSystem;
+import io.github.StarvingValley.models.systems.HungerRenderSystem;
+import io.github.StarvingValley.models.systems.HungerSystem;
+import io.github.StarvingValley.models.systems.MapRenderSystem;
+import io.github.StarvingValley.models.systems.MovementSystem;
+import io.github.StarvingValley.models.systems.RenderSystem;
+import io.github.StarvingValley.models.systems.SpriteSystem;
+import io.github.StarvingValley.models.systems.SyncMarkingSystem;
+import io.github.StarvingValley.models.systems.VelocitySystem;
+import io.github.StarvingValley.models.types.GameContext;
+import io.github.StarvingValley.models.types.WorldLayer;
+import io.github.StarvingValley.utils.MapUtils;
+
+public class VillageController {
+
+    private final Engine engine;
+    private final SpriteBatch batch;
+    private final EventBus eventBus;
+    private final AssetManager assetManager;
+    private final IFirebaseRepository firebaseRepository;
+
+    public GameContext gameContext;
+
+    private Entity camera;
+    private Entity map;
+
+    public VillageController(IFirebaseRepository firebaseRepository, EventBus eventBus, AssetManager assetManager) {
+        this.firebaseRepository = firebaseRepository;
+        this.eventBus = eventBus;
+        this.assetManager = assetManager;
+        this.engine = new Engine();
+        this.batch = new SpriteBatch();
+        this.gameContext = new GameContext();
+        this.gameContext.spriteBatch = this.batch;
+        this.gameContext.eventBus = this.eventBus;
+        this.gameContext.assetManager = this.assetManager;
+        this.gameContext.firebaseRepository = this.firebaseRepository;
+        this.gameContext.engine = this.engine;
+        initGame();
+    }
+
+    private void initGame() {
+        int tilesWide = Config.CAMERA_TILES_WIDE;
+        float tilesHigh = MapUtils.calculateVerticalTileCount(tilesWide);
+
+        camera = CameraFactory.createCamera(tilesWide, tilesHigh);
+        CameraComponent cameraComponent = Mappers.camera.get(camera);
+
+        gameContext.camera = cameraComponent.camera;
+
+        map = MapFactory.createMap("TownMap.tmx", Config.UNIT_SCALE, cameraComponent);
+
+        engine.addEntity(camera);
+        engine.addEntity(map);
+
+        engine.addSystem(new ClickSystem(gameContext));
+        engine.addSystem(new MapRenderSystem());
+        engine.addSystem(new VelocitySystem());
+        engine.addSystem(new EnvironmentCollisionSystem());
+        engine.addSystem(new MovementSystem(gameContext));
+        engine.addSystem(new CameraSystem());
+        engine.addSystem(new RenderSystem(gameContext));
+        engine.addSystem(new HungerSystem(gameContext));
+        engine.addSystem(new SpriteSystem(gameContext));
+        engine.addSystem(new HungerRenderSystem(gameContext));
+        engine.addSystem(new SyncMarkingSystem(gameContext));
+        engine.addSystem(new FirebaseSyncSystem(gameContext));
+        engine.addSystem(new ClickedCleanupSystem());
+        engine.addSystem(new EventCleanupSystem(gameContext));
+
+        TiledMapComponent tiledMap = Mappers.tiledMap.get(map);
+        MapUtils.loadEnvCollidables(tiledMap.tiledMap, Config.UNIT_SCALE, engine);
+        MapUtils.loadPlacementBlockers(tiledMap.tiledMap, Config.UNIT_SCALE, WorldLayer.TERRAIN, engine);
+
+        MapUtils.loadSyncedEntities(gameContext, getCamera());
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public SpriteBatch getBatch() {
+        return batch;
+    }
+
+    public Entity getCamera() {
+        return camera;
+    }
+
+    public void dispose() {
+        batch.dispose();
+    }
+}
---
+++ core/src/main/java/io/github/StarvingValley/views/VillageView.java
@@ -0,0 +1,133 @@
+package io.github.StarvingValley.views;
+
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+
+import io.github.StarvingValley.controllers.InputEventController;
+import io.github.StarvingValley.controllers.JoystickController;
+import io.github.StarvingValley.controllers.StarvingValley;
+import io.github.StarvingValley.controllers.VillageController;
+import io.github.StarvingValley.models.Mappers;
+import io.github.StarvingValley.models.Interfaces.IFirebaseRepository;
+import io.github.StarvingValley.models.components.CameraComponent;
+import io.github.StarvingValley.models.events.EventBus;
+import io.github.StarvingValley.models.types.PrefabType;
+import io.github.StarvingValley.utils.BuildUtils;
+import io.github.StarvingValley.utils.EventDebugger;
+import io.github.StarvingValley.utils.MapUtils;
+
+public class VillageView extends ScreenAdapter {
+    public AssetManager assetManager;
+    IFirebaseRepository _firebaseRepository;
+    private JoystickOverlay joystickOverlay;
+    private InputAdapter inputAdapter; // temp
+    private InputEventAdapter inputEventAdapter;
+
+    private EventBus eventBus;
+    private VillageController controller;
+    private Engine engine;
+
+    private final EventDebugger eventDebugger;
+    private EventDebugOverlay eventDebugOverlay;
+
+    public VillageView(IFirebaseRepository firebaseRepository, StarvingValley game, Entity player) {
+        _firebaseRepository = firebaseRepository;
+        eventDebugger = new EventDebugger();
+        eventDebugOverlay = new EventDebugOverlay(eventDebugger);
+        eventBus = new EventBus(eventDebugger);
+
+        // pre-load some assets that we know we always need.
+        // Potentially add assetManager.finishLoading(); to wait
+        assetManager = new AssetManager();
+        assetManager.load("DogBasic.png", Texture.class);
+        assetManager.load("tomato1.png", Texture.class);
+        assetManager.load("potato1.png", Texture.class);
+        assetManager.load("dirt.png", Texture.class);
+
+        // TODO: Temp logic. When inventory is implemented it should handle this, and it
+        //  should only be possible on entities
+        //  with BuildableComponent. Use BuildUtils.isBuildable
+        inputAdapter = new InputAdapter() {
+            @Override
+            public boolean keyDown(int keycode) {
+                PrefabType prefabType = null;
+
+                switch (keycode) {
+                    case Input.Keys.C:
+                        prefabType = PrefabType.TOMATO_CROP;
+                        break;
+                    case Input.Keys.E:
+                        prefabType = PrefabType.POTATO_CROP;
+                        break;
+                    case Input.Keys.F:
+                        prefabType = PrefabType.SOIL;
+                        break;
+                }
+
+                if (prefabType != null) {
+                    BuildUtils.toggleBuildPreview(prefabType, engine);
+                }
+
+                return true;
+            }
+        };
+
+        controller = new VillageController(_firebaseRepository, eventBus, assetManager);
+        // to avoid
+        // problems with the temporal
+        // input handling
+        engine = controller.getEngine();
+
+        CameraComponent cameraComponent = Mappers.camera.get(controller.getCamera());
+        inputEventAdapter = new InputEventAdapter(new InputEventController(cameraComponent.camera, eventBus));
+    }
+
+    @Override
+    public void show() {
+
+        JoystickController joystickController = new JoystickController(engine);
+        joystickOverlay = new JoystickOverlay(joystickController);
+
+        InputAdapter joystickInputAdapter = joystickOverlay.getInputAdapter();
+
+        InputMultiplexer multiplexer = new InputMultiplexer();
+        multiplexer.addProcessor(inputEventAdapter);
+        multiplexer.addProcessor(inputAdapter); // temp
+        multiplexer.addProcessor(joystickInputAdapter);
+
+        Gdx.input.setInputProcessor(multiplexer);
+    }
+
+    @Override
+    public void render(float delta) {
+        assetManager.update();
+
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        Gdx.gl.glEnable(GL20.GL_BLEND);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+
+        CameraComponent cameraComponent = Mappers.camera.get(controller.getCamera());
+        if (cameraComponent != null) {
+            controller.getBatch().setProjectionMatrix(cameraComponent.camera.combined);
+        }
+
+        engine.update(delta);
+        joystickOverlay.render();
+        eventDebugOverlay.render();
+    }
+
+    @Override
+    public void dispose() {
+        controller.dispose();
+    }
+}
