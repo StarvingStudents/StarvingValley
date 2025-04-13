afd15fe6a6cb4a0abc9e2e696551648390134a55
Kristian Seon-Ho Kim <kskim@stud.ntnu.no>
2025-04-13 21:25:48 +0200

    Add EatingSystem

--- core/src/main/java/io/github/StarvingValley/models/systems/EatingSystem.java
+++ core/src/main/java/io/github/StarvingValley/models/systems/EatingSystem.java
@@ -1,11 +1,17 @@
 package io.github.StarvingValley.models.systems;
 
+import java.util.List;
+
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
 
+import io.github.StarvingValley.models.components.ButtonComponent;
+import io.github.StarvingValley.models.components.ClickableComponent;
+import io.github.StarvingValley.models.components.ClickedComponent;
 import io.github.StarvingValley.models.components.EatingComponent;
 import io.github.StarvingValley.models.components.HungerComponent;
+import io.github.StarvingValley.models.events.EatingButtonPressedEvent;
 import io.github.StarvingValley.models.events.EntityUpdatedEvent;
 import io.github.StarvingValley.models.events.EventBus;
 import io.github.StarvingValley.utils.DiffUtils;
@@ -14,20 +20,24 @@ public class EatingSystem extends IteratingSystem {
     private EventBus eventBus;
 
     public EatingSystem(EventBus eventBus) {
-        super(Family.all(HungerComponent.class, EatingComponent.class).get());
+        super(Family.all(HungerComponent.class).get());
         this.eventBus = eventBus;
     }
 
     @Override
     protected void processEntity(Entity entity, float deltaTime) {
-        HungerComponent hunger = entity.getComponent(HungerComponent.class);
-        EatingComponent eating = entity.getComponent(EatingComponent.class);
+        List<EatingButtonPressedEvent> events = eventBus.getEvents(EatingButtonPressedEvent.class);
+        if (events.size() > 0) {
+            System.out.println(events.get((0)));
 
-        float oldHunger = hunger.hungerPoints;
+            // HungerComponent hunger = entity.getComponent(HungerComponent.class);
+            // float oldHunger = hunger.hungerPoints;
 
-        hunger.hungerPoints = Math.min(hunger.maxHungerPoints, hunger.hungerPoints + eating.foodPoints); 
+            // hunger.hungerPoints = Math.min(hunger.maxHungerPoints, hunger.hungerPoints +
+            // events.get(0).foodPoints);
 
-        if (DiffUtils.hasChanged(hunger.hungerPoints, oldHunger))
-            eventBus.publish(new EntityUpdatedEvent(entity));
+            // if (DiffUtils.hasChanged(hunger.hungerPoints, oldHunger))
+            // eventBus.publish(new EntityUpdatedEvent(entity));
+        }
     }
 }
