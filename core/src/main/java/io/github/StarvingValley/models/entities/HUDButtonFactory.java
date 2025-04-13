7f51e9165edac29f0e7f3a67a76a7977ee93322c
Kristian Seon-Ho Kim <kskim@stud.ntnu.no>
2025-04-13 21:25:09 +0200

    Add HUDButtonFactory

--- core/src/main/java/io/github/StarvingValley/models/entities/HUDButtonFactory.java
+++ core/src/main/java/io/github/StarvingValley/models/entities/HUDButtonFactory.java
@@ -1,21 +1,12 @@
 package io.github.StarvingValley.models.entities;
 
 import com.badlogic.ashley.core.Entity;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
-
-import io.github.StarvingValley.config.Config;
 import io.github.StarvingValley.models.components.ButtonComponent;
 import io.github.StarvingValley.models.components.ClickableComponent;
-import io.github.StarvingValley.models.components.ClickedComponent;
 import io.github.StarvingValley.models.components.PositionComponent;
 import io.github.StarvingValley.models.components.SizeComponent;
 import io.github.StarvingValley.models.components.SpriteComponent;
 import io.github.StarvingValley.models.types.GameContext;
-import io.github.StarvingValley.utils.ScreenUtils;
 import io.github.StarvingValley.models.types.ButtonType;
 
 public class HUDButtonFactory {
@@ -25,7 +16,6 @@ public class HUDButtonFactory {
         Entity button = new Entity();
 
         button.add(new ClickableComponent())
-                // .add(new ClickedComponent())
                 .add(new SizeComponent(width, height))
                 .add(new PositionComponent(x, y))
                 .add(new SpriteComponent(texturePath))
