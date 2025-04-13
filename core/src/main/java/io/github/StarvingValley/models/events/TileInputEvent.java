972cc2980caea361bfa8ecf7fb0d94137c95cdc1
Kristian Seon-Ho Kim <kskim@stud.ntnu.no>
2025-04-13 22:05:53 +0200

    Work on handling HUD button presses

--- core/src/main/java/io/github/StarvingValley/models/events/TileInputEvent.java
+++ core/src/main/java/io/github/StarvingValley/models/events/TileInputEvent.java
@@ -1,15 +1,18 @@
 package io.github.StarvingValley.models.events;
 
 import com.badlogic.gdx.math.GridPoint2;
+import com.badlogic.gdx.math.Vector2;
 
 import io.github.StarvingValley.models.Interfaces.Event;
 
 public abstract class TileInputEvent implements Event {
     public final GridPoint2 tile;
+    public final Vector2 screenPos;
     public final int button;
 
-    public TileInputEvent(GridPoint2 tile, int button) {
+    public TileInputEvent(GridPoint2 tile, Vector2 screenPos, int button) {
         this.tile = tile;
         this.button = button;
+        this.screenPos = screenPos;
     }
 }
--- core/src/main/java/io/github/StarvingValley/models/systems/HUDButtonPressSystem.java
+++ core/src/main/java/io/github/StarvingValley/models/systems/HUDButtonPressSystem.java
@@ -30,12 +30,16 @@ public class HUDButtonPressSystem extends EntitySystem {
 
     @Override
     public void update(float delta) {
-        List<TapEvent> tapEvents = context.eventBus.getEvents(TapEvent.class);
+        // List<TapEvent> tapEvents = context.eventBus.getEvents(TapEvent.class);
 
-        ImmutableArray<Entity> clickableEntities = getEngine()
-                .getEntitiesFor(
-                        Family.all(ButtonComponent.class, ClickableComponent.class)
-                                .get());
+        // ImmutableArray<Entity> clickableEntities = getEngine()
+        //         .getEntitiesFor(
+        //                 Family.all(ButtonComponent.class, ClickableComponent.class)
+        //                         .get());
+
+        // ImmutableArray<Entity> clickableWorldEntities = getEngine().getEntitiesFor(
+        // Family.all(PositionComponent.class, SizeComponent.class, ClickableComponent.class)
+        //     .exclude(ButtonComponent.class).get());
 
         // for (TapEvent clickEvent : events) {
         //     for (Entity entity : clickableEntities) {
@@ -48,31 +52,77 @@ public class HUDButtonPressSystem extends EntitySystem {
         //         }
         //     }
         // }
+    // }
+
+//     for(TapEvent event:tapEvents)
+//     {
+//         boolean handledHud = attachComponentIfOverlapping(clickableEntities, event.screenPos, ClickedComponent::new);
+//         if (!handledHud) {
+//         attachComponentIfOverlapping(clickableWorldEntities, event.tile, ClickedComponent::new);
+//         }
+//     }
+// }
 
 
-        for (TapEvent event : tapEvents) {
-            boolean handledHud = attachComponentIfOverlapping(clickableEntities, event.screenPos, ClickedComponent::new);
-    }
-
-    private boolean attachComponentIfOverlapping(
-            ImmutableArray<Entity> entities,
-            Vector2 screenPos,
-            Supplier<Component> componentSupplier) {
-        boolean handled = false;
-        for (Entity entity : entities) {
-            PositionComponent pos = Mappers.position.get(entity);
-            SizeComponent size = Mappers.size.get(entity);
-
-            boolean isInside = screenPos.x >= pos.position.x &&
-                    screenPos.x <= pos.position.x + size.width &&
-                    screenPos.y >= pos.position.y &&
-                    screenPos.y <= pos.position.y + size.height;
-
-            if (isInside) {
-                entity.add(componentSupplier.get());
-                handled = true;
-            }
+        processTapEvents();
         }
-        return handled;
+
+  private void processTapEvents() {
+    List<TapEvent> tapEvents = context.eventBus.getEvents(TapEvent.class);
+
+    ImmutableArray<Entity> clickableHudEntities = getEngine().getEntitiesFor(
+        Family.all(PositionComponent.class, SizeComponent.class, ClickableComponent.class, ButtonComponent.class).get());
+
+    ImmutableArray<Entity> clickableWorldEntities = getEngine().getEntitiesFor(
+        Family.all(PositionComponent.class, SizeComponent.class, ClickableComponent.class)
+            .exclude(ButtonComponent.class).get());
+
+    for (TapEvent event : tapEvents) {
+      boolean handledHud = attachComponentIfOverlapping(clickableHudEntities, event.screenPos, ClickedComponent::new);
+      if (!handledHud) {
+        attachComponentIfOverlapping(clickableWorldEntities, event.tile, ClickedComponent::new);
+      }
     }
+  }
+
+  
+
+  private boolean attachComponentIfOverlapping(
+      ImmutableArray<Entity> entities,
+      GridPoint2 tile,
+      Supplier<Component> componentSupplier) {
+    boolean handled = false;
+    for (Entity entity : entities) {
+      PositionComponent pos = Mappers.position.get(entity);
+      SizeComponent size = Mappers.size.get(entity);
+
+      if (TileUtils.isOverlappingTile(pos.position.x, pos.position.y, size.width, size.height, tile)) {
+        entity.add(componentSupplier.get());
+        handled = true;
+      }
+    }
+    return handled;
+  }
+
+  private boolean attachComponentIfOverlapping(
+      ImmutableArray<Entity> entities,
+      Vector2 screenPos,
+      Supplier<Component> componentSupplier) {
+    boolean handled = false;
+    for (Entity entity : entities) {
+      PositionComponent pos = Mappers.position.get(entity);
+      SizeComponent size = Mappers.size.get(entity);
+
+      boolean isInside = screenPos.x >= pos.position.x &&
+          screenPos.x <= pos.position.x + size.width &&
+          screenPos.y >= pos.position.y &&
+          screenPos.y <= pos.position.y + size.height;
+
+      if (isInside) {
+        entity.add(componentSupplier.get());
+        handled = true;
+      }
+    }
+    return handled;
+  }
 }
