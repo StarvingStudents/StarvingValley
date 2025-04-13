b20f61cfcfe177171b846bf7299d8f3986ef9c22
Kristian Seon-Ho Kim <kskim@stud.ntnu.no>
2025-04-13 22:27:06 +0200

    Remove TouchDownEvent

--- core/src/main/java/io/github/StarvingValley/models/events/TouchDownEvent.java
+++ core/src/main/java/io/github/StarvingValley/models/events/TouchDownEvent.java
@@ -1,9 +1,9 @@
-package io.github.StarvingValley.models.events;
+// package io.github.StarvingValley.models.events;
 
-import com.badlogic.gdx.math.GridPoint2;
+// import com.badlogic.gdx.math.GridPoint2;
 
-public class TouchDownEvent extends TileInputEvent {
-    public TouchDownEvent(GridPoint2 tile, int button) {
-        super(tile, button);
-    }
-}
+// public class TouchDownEvent extends TileInputEvent {
+//     public TouchDownEvent(GridPoint2 tile, int button) {
+//         super(tile, button);
+//     }
+// }
