519633947162cb23db97543ef5274759dd71471a
Kristian Seon-Ho Kim <kskim@stud.ntnu.no>
2025-04-14 12:27:15 +0200

    Add InventoryItemComponent from Inventory system branch

--- core/src/main/java/io/github/StarvingValley/models/components/InventoryItemComponent.java
+++ core/src/main/java/io/github/StarvingValley/models/components/InventoryItemComponent.java
@@ -2,6 +2,18 @@ package io.github.StarvingValley.models.components;
 
 import com.badlogic.ashley.core.Component;
 
-public class InventoryItemComponent implements Component {
+import io.github.StarvingValley.models.types.PrefabType;
 
+public class InventoryItemComponent implements Component {
+    public PrefabType type;
+    public int quantity;
+    public int slotX;
+    public int slotY;
+
+    public InventoryItemComponent(PrefabType type, int quantity, int slotX, int slotY) {
+        this.type = type;
+        this.quantity = quantity;
+        this.slotX = slotX;
+        this.slotY = slotY;
+    }
 }
