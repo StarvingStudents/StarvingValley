83fc0fc27113d5a8f2a77c1d08b4b9e94d00c89d
Kristian Seon-Ho Kim <kskim@stud.ntnu.no>
2025-04-16 09:44:04 +0200

    Add placeholder logic for handling map transition buttons

--- core/src/main/java/io/github/StarvingValley/models/systems/HUDButtonPressHandlingSystem.java
+++ core/src/main/java/io/github/StarvingValley/models/systems/HUDButtonPressHandlingSystem.java
@@ -6,6 +6,8 @@ import com.badlogic.ashley.systems.IteratingSystem;
 import io.github.StarvingValley.models.events.EatingButtonPressedEvent;
 import io.github.StarvingValley.models.types.ButtonType;
 import io.github.StarvingValley.models.types.GameContext;
+import io.github.StarvingValley.models.types.ScreenType;
+import io.github.StarvingValley.controllers.StarvingValley;
 import io.github.StarvingValley.models.Mappers;
 import io.github.StarvingValley.models.components.ButtonComponent;
 import io.github.StarvingValley.models.components.ClickedComponent;
@@ -13,10 +15,12 @@ import io.github.StarvingValley.models.components.ClickedComponent;
 public class HUDButtonPressHandlingSystem extends IteratingSystem {
 
     private GameContext context;
+    private StarvingValley game;
 
-    public HUDButtonPressHandlingSystem(GameContext context) {
+    public HUDButtonPressHandlingSystem(GameContext context, StarvingValley game) {
         super(Family.all(ButtonComponent.class, ClickedComponent.class).get());
         this.context = context;
+        this.game = game;
     }
 
     @Override
@@ -31,6 +35,14 @@ public class HUDButtonPressHandlingSystem extends IteratingSystem {
 
         if (button.buttonType == ButtonType.EATING_BUTTON) {
             context.eventBus.publish(new EatingButtonPressedEvent());
+        }
+
+        if (button.buttonType == ButtonType.FARM_TO_WORLD_MAP_BUTTON) {
+            // TODO: use requestViewSwitch to move to world map
+        }
+
+        if (button.buttonType == ButtonType.WORLD_MAP_TO_FARM_BUTTON) {
+            // TODO: use requestViewSwitch to move to farm
         }
     }
 }
